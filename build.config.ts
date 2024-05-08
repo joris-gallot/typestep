8a8d3688bd1036b264f3dc3bd9eacd20065c99fc
Joris <joris.gallot18@gmail.com>
2024-05-08 17:10:58 +0200

    chore: remove index file from exported files

--- build.config.ts
+++ build.config.ts
@@ -1,12 +1,5 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
-  entries: ['src/index.ts', 'src/cli.ts'],
-  declaration: true,
-  rollup: {
-    emitCJS: true,
-    esbuild: {
-      target: 'esnext',
-    },
-  },
+  entries: ['src/cli.ts'],
 })
