28e843f5b8a718e7d6a88b2a713e57d7e681211d
Joris <joris.gallot18@gmail.com>
2025-04-25 21:38:56 +0200

    chore: move playground at root

--- playground.ts
+++ playground.ts
@@ -1,12 +1,12 @@
 /* eslint-disable no-console */
 import { readFile } from 'node:fs/promises'
 import { resolve } from 'node:path'
-import { parseTscOutput } from './index.js'
+import { parseTscOutput } from './src/index.js'
 
 async function run() {
   let tscOutput
   try {
-    tscOutput = await readFile(resolve(__dirname, '..', 'tsc-output.log'), 'utf8')
+    tscOutput = await readFile(resolve(__dirname, 'tsc-output.log'), 'utf8')
   }
   catch (error) {
     throw new Error('Could not read tsc-output.log', { cause: error })
